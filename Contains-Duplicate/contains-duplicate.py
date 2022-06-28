0b0459187a30e6c669a373960ee82d8b8d7838c5
Sargun Singh Sethi <100695155+SargunSinghSethi@users.noreply.github.com>
2022-06-28 19:34:32 +0530

    Create contains-duplicate.py

---
+++ Contains-Duplicate/contains-duplicate.py
@@ -0,0 +1,44 @@
+# Contains Duplicate (Arrays)
+# Google, Amazon, Microsoft, Adobe, TCS, Bloomberg, Apple, Uber
+
+# Given an integer array 'nums’, return 1(true) if any value appears at least twice in the array, and return 0(false) if every element is distinct.
+
+
+# Input Format
+# First Parameter - Number n
+# Second Parameter - Array of Integer arr
+
+
+# Output Format
+# Return the integer(0 or 1)
+
+
+# Example 1:
+# Input:
+#     4
+#     1 2 3 1
+# Output:
+#     1
+# Explanation:
+#     '1' is repeated two times.
+
+
+# Example 2:
+# Input:
+#     4
+#     1 2 3 4 
+# Output:
+#     0
+# Explanation:
+#     No values are repeated.
+
+
+# Constraints:
+# 1 <= nums.length <= 105
+# -10^9 <= nums[i] <= 10^9
+
+def solve(n, arr):
+    s = list(set(arr))
+    if len(s) < len(arr):
+        return 1
+    return 0
